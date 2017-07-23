ebee855f4532641a553357ecf326236ca1efce7e
luomichelle <mluo0301@gmail.com>
2017-07-22 17:42:20 -0700

    adding notes

--- src/app/containers/main.ts
+++ src/app/containers/main.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+  
 @Component({
   selector: 'main-container',
   template: `
     <div class="main-container">
       <app-bar></app-bar>
       <main class="main">
-        content will go here
+        <notes-container></notes-container>
       </main>
     </div>
   `
